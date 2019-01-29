300af490f7ef3920c0b1c777fb6469bb9711545b
will <will.pankiewicz@gmail.com>
2019-01-28 20:32:58 -0800

    Added scn-api backend

---
+++ scn-api/src/resolvers/Query.js
@@ -0,0 +1,26 @@
+async function feed(parent, args, context) {
+  const where = args.filter
+    ? {
+        OR: [
+          { description_contains: args.filter },
+          { url_contains: args.filter },
+        ],
+      }
+    : {}
+
+  const links = await context.prisma.links({
+    where,
+    skip: args.skip,
+    first: args.first,
+    orderBy: args.orderBy,
+  })
+  const count = links.length
+  return {
+    links,
+    count,
+  }
+}
+
+module.exports = {
+  feed,
+}
